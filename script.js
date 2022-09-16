dcf5220aa3083cd761789269c83c7ac9db970232
buddhi <bagun2@student.monash.edu>
2022-09-16 12:09:04 +1000

    Pushing new code

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!");
+// Notes
+// Need to attach action to button so user can save tabs.
+
+urlArray = [];
+
+chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+    for (let i = 0; i < tabs.length; i++) {
+        urlArray.push(
+            `<li>${tabs[i].title}: ${tabs[i].url}</li>`
+        )
+    }
+    document.write(urlArray.toString());
+});
+
+// chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+//     document.write('<h3>These are your current tabs</h3>');
+//     document.write('<ul>');
+//     for (let i = 0; i < tabs.length; i++) {
+//         document.write(`<li>${tabs[i].title}: ${tabs[i].url}</li>`)
+//     }
+//     document.write('</ul>')
+// });
