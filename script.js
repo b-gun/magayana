023f4c6b763b56f30cf6e72513e55e0c88565029
buddhi <bagun2@student.monash.edu>
2022-09-17 12:15:35 +1000

    Added firing off trigger connected to button.

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 // Notes
-// Need to attach action to button so user can save tabs.
 
 urlArray = [];
 
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-    for (let i = 0; i < tabs.length; i++) {
-        urlArray.push(
-            `<li>${tabs[i].title}: ${tabs[i].url}</li>`
-        )
-    }
-    document.write(urlArray.toString());
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('add');
 
-// chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-//     document.write('<h3>These are your current tabs</h3>');
-//     document.write('<ul>');
-//     for (let i = 0; i < tabs.length; i++) {
-//         document.write(`<li>${tabs[i].title}: ${tabs[i].url}</li>`)
-//     }
-//     document.write('</ul>')
-// });
+    button.addEventListener('click', function() {
+        chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+            for (let i = 0; i < tabs.length; i++) {
+                urlArray.push(
+                    `<li>${tabs[i].title}: ${tabs[i].url}</li>`
+                )
+            }
+            document.getElementById('saved').innerText = urlArray.toString();
+        });
+    });
+})
