f217a98dfa4df846cf04cf2530a535e48e3aef70
buddhi <bagun2@student.monash.edu>
2022-10-17 16:31:40 +1100

    Added listener for 'menu'

--- script.js
+++ script.js
@@ -10,71 +10,6 @@
 
 // After the top three are done, V1.0 is finished.
 
-        
-// let urlArray = [];
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const addTabs = document.getElementById('add');
-
-//     addTabs.addEventListener('click', function() {
-//         const journeyName = document.getElementById('journeyName').value;
-//         let openRequest = indexedDB.open("magayana", 1);
-
-        // openRequest.onupgradeneeded = function() {
-        //     let db = openRequest.result;
-
-        //     if (!db.objectStoreNames.contains("sites")) { 
-        //         db.createObjectStore("sites", {autoIncrement: true}); 
-        //     }
-        // };
-
-//         openRequest.onsuccess = function() {
-//             let db = openRequest.result;
-            
-            // chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-            //     let transaction = db.transaction("sites", "readwrite");
-                          
-            //     for (let i = 0; i < tabs.length; i++) {
-            //         let links = transaction.objectStore("sites");
-
-            //         let request = links.add({
-            //             journeyName: journeyName,
-            //             name: tabs[i].title,
-            //             link: tabs[i].url
-            //         });
-
-            //         request.onsuccess = function() {
-            //             console.log("Link succesfully added to store.")
-            //         }
-
-            //         request.onerror = function() {
-            //             console.log("There was an error", request.error)
-            //         }
-            //     }
-            // });
-
-            // let transaction = db.transaction("sites", "readonly");
-            // let siteObject = transaction.objectStore("sites");
-            // let sites = siteObject.getAll();
-
-//             sites.onsuccess = function(event) {
-//                 const saved = document.getElementById('saved');
-//                 saved.textContent = '';
-//                 linkArray = sites.result;
-//                 let journeyNamesArray = linkArray.map(x => x.journeyName);
-//                 journeyNamesArray = [...new Set(journeyNamesArray)];
-
-                // journeyNamesArray.forEach(x => {
-                //     const div = document.createElement("div")
-                //     div.className = 'journey';
-                //     div.innerHTML = x;
-                //     saved.appendChild(div);
-                // });                
-//             };
-//         }
-//     });
-// })
-
 document.addEventListener('DOMContentLoaded', function() {
     let openRequest = indexedDB.open("magayana", 1);
 
@@ -90,17 +25,52 @@ document.addEventListener('DOMContentLoaded', function() {
         const addTabs = document.getElementById('add');
         init(openRequest);
 
+        waitForElm('p').then((elm) => {
+            const links = document.querySelectorAll('p');
+
+            for (let i = 0; i < links.length; i++) {
+                links[i].addEventListener('click', alertClick);
+            }
+        });
+
         addTabs.addEventListener('click', function() {
             const journeyName = document.getElementById('journeyName').value;
             save(openRequest, journeyName);
+            // chrome.windows.create({url: ['http://www.google.com', 'https://www.theage.com.au/']});             
+        });  
 
-        });    
     };
 });
+
+
 
 // ***************
 // Functions
 // ***************
+
+function alertClick() {
+    console.log(this.id); 
+}
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
 
 // Initialize extension and show all saved journeys.
 function init(openRequest) {
@@ -118,8 +88,8 @@ function init(openRequest) {
         journeyNamesArray.forEach(x => {
             const div = document.createElement("div")
             div.className = 'journey';
-            div.innerHTML = x;
-            saved.appendChild(div);
+            div.innerHTML = `<p id='${x}'>${x}</p>`
+            saved.appendChild(div);         
         });          
     } 
 }
